d81e945168d82852702cfea5781440f85a60ff35
Doug Kent <github@douglaskent.com>
2018-04-26 10:14:41 -0400

    uncommented tests

--- test/vestingScheme.ts
+++ test/vestingScheme.ts
@@ -40,148 +40,148 @@ describe("VestingScheme scheme", () => {
 
   });
 
-  // it("can get DAO's agreements", async () => {
+  it("can get DAO's agreements", async () => {
 
-  //   const options = {
-  //     amountPerPeriod: web3.toWei(10),
-  //     beneficiaryAddress: accounts[1],
-  //     cliffInPeriods: 0,
-  //     numOfAgreedPeriods: 1,
-  //     periodLength: 1,
-  //     returnOnCancelAddress: helpers.SOME_ADDRESS,
-  //     signaturesReqToCancel: 3,
-  //     signers: [accounts[0], accounts[1], accounts[2], accounts[3]],
-  //   };
+    const options = {
+      amountPerPeriod: web3.toWei(10),
+      beneficiaryAddress: accounts[1],
+      cliffInPeriods: 0,
+      numOfAgreedPeriods: 1,
+      periodLength: 1,
+      returnOnCancelAddress: helpers.SOME_ADDRESS,
+      signaturesReqToCancel: 3,
+      signers: [accounts[0], accounts[1], accounts[2], accounts[3]],
+    };
 
-  //   const result = await vestingScheme.propose(Object.assign({ avatar: dao.avatar.address }, options));
+    const result = await vestingScheme.propose(Object.assign({ avatar: dao.avatar.address }, options));
 
-  //   const proposalId1 = result.proposalId;
+    const proposalId1 = result.proposalId;
 
-  //   const result2 = await vestingScheme.propose(Object.assign({ avatar: dao.avatar.address }, options));
-  //   const proposalId2 = result2.proposalId;
+    const result2 = await vestingScheme.propose(Object.assign({ avatar: dao.avatar.address }, options));
+    const proposalId2 = result2.proposalId;
 
-  //   let proposalService = vestingScheme.createProposalService();
+    let proposalService = vestingScheme.createProposalService();
 
-  //   let agreements = await proposalService.getProposals({ avatarAddress: dao.avatar.address });
+    let agreements = await proposalService.getProposals({ avatarAddress: dao.avatar.address });
 
-  //   assert(agreements.length === 2, "Should have found at 2 agreements");
-  //   assert(agreements.filter((a: Agreement) => a.proposalId === proposalId1).length, "proposalId1 not found");
-  //   assert(agreements.filter((a: Agreement) => a.proposalId === proposalId2).length, "proposalId2 not found");
+    assert(agreements.length === 2, "Should have found at 2 agreements");
+    assert(agreements.filter((a: Agreement) => a.proposalId === proposalId1).length, "proposalId1 not found");
+    assert(agreements.filter((a: Agreement) => a.proposalId === proposalId2).length, "proposalId2 not found");
 
-  //   agreements = await proposalService.getProposals({ avatarAddress: dao.avatar.address, eventArgsFilter: { _proposalId: proposalId2 } });
+    agreements = await proposalService.getProposals({ avatarAddress: dao.avatar.address, eventArgsFilter: { _proposalId: proposalId2 } });
 
-  //   // TODO: this should be 1, see https://github.com/daostack/arc/issues/448
-  //   assert.equal(agreements.length, 2, "Should have found 1 agreements");
-  //   assert(agreements.filter((p: Agreement) => p.proposalId === proposalId2).length, "proposalId2 not found");
-  //   assert.equal(agreements[0].beneficiaryAddress, accounts[1], "beneficiaryAddress not set properly on agreement");
-  // });
+    // TODO: this should be 1, see https://github.com/daostack/arc/issues/448
+    assert.equal(agreements.length, 2, "Should have found 1 agreements");
+    assert(agreements.filter((p: Agreement) => p.proposalId === proposalId2).length, "proposalId2 not found");
+    assert.equal(agreements[0].beneficiaryAddress, accounts[1], "beneficiaryAddress not set properly on agreement");
+  });
 
-  // it("can collect on the agreement", async () => {
+  it("can collect on the agreement", async () => {
 
-  //   const options = {
-  //     amountPerPeriod: web3.toWei(10),
-  //     beneficiaryAddress: accounts[0],
-  //     cliffInPeriods: 0,
-  //     numOfAgreedPeriods: 1,
-  //     periodLength: 1,
-  //     returnOnCancelAddress: helpers.SOME_ADDRESS,
-  //     signaturesReqToCancel: 1,
-  //     signers: [accounts[0]],
-  //     token: await dao.token.address,
-  //   };
+    const options = {
+      amountPerPeriod: web3.toWei(10),
+      beneficiaryAddress: accounts[0],
+      cliffInPeriods: 0,
+      numOfAgreedPeriods: 1,
+      periodLength: 1,
+      returnOnCancelAddress: helpers.SOME_ADDRESS,
+      signaturesReqToCancel: 1,
+      signers: [accounts[0]],
+      token: await dao.token.address,
+    };
 
-  //   const result = await createAgreement(options);
-  //   const agreementId = result.agreementId;
+    const result = await createAgreement(options);
+    const agreementId = result.agreementId;
 
-  //   // this will mine a block, allowing the award to be redeemed
-  //   await helpers.increaseTime(1);
+    // this will mine a block, allowing the award to be redeemed
+    await helpers.increaseTime(1);
 
-  //   const result2 = await vestingScheme.collect({ agreementId });
+    const result2 = await vestingScheme.collect({ agreementId });
 
-  //   assert.isOk(result2);
-  //   assert.isOk(result2.tx);
-  //   assert.equal(result2.tx.logs.length, 1); // no other event
-  //   assert.equal(result2.tx.logs[0].event, "Collect");
-  // });
+    assert.isOk(result2);
+    assert.isOk(result2.tx);
+    assert.equal(result2.tx.logs.length, 1); // no other event
+    assert.equal(result2.tx.logs[0].event, "Collect");
+  });
 
-  // it("revert sign to cancel agreement", async () => {
+  it("revert sign to cancel agreement", async () => {
 
-  //   const options = {
-  //     amountPerPeriod: web3.toWei(10),
-  //     beneficiaryAddress: helpers.SOME_ADDRESS,
-  //     cliffInPeriods: 0,
-  //     numOfAgreedPeriods: 1,
-  //     periodLength: 1,
-  //     returnOnCancelAddress: helpers.SOME_ADDRESS,
-  //     signaturesReqToCancel: 2,
-  //     signers: [accounts[0], accounts[1]],
-  //     token: await dao.token.address,
-  //   };
+    const options = {
+      amountPerPeriod: web3.toWei(10),
+      beneficiaryAddress: helpers.SOME_ADDRESS,
+      cliffInPeriods: 0,
+      numOfAgreedPeriods: 1,
+      periodLength: 1,
+      returnOnCancelAddress: helpers.SOME_ADDRESS,
+      signaturesReqToCancel: 2,
+      signers: [accounts[0], accounts[1]],
+      token: await dao.token.address,
+    };
 
-  //   const result = await createAgreement(options);
-  //   const agreementId = result.agreementId;
+    const result = await createAgreement(options);
+    const agreementId = result.agreementId;
 
-  //   let result2 = await vestingScheme.signToCancel({ agreementId });
+    let result2 = await vestingScheme.signToCancel({ agreementId });
 
-  //   assert.isOk(result2);
-  //   assert.isOk(result2.tx);
-  //   assert.equal(result2.tx.logs.length, 1); // no cancelled event
-  //   assert.equal(result2.tx.logs[0].event, "SignToCancelAgreement");
+    assert.isOk(result2);
+    assert.isOk(result2.tx);
+    assert.equal(result2.tx.logs.length, 1); // no cancelled event
+    assert.equal(result2.tx.logs[0].event, "SignToCancelAgreement");
 
-  //   result2 = await vestingScheme.revokeSignToCancel({ agreementId });
+    result2 = await vestingScheme.revokeSignToCancel({ agreementId });
 
-  //   assert.isOk(result2);
-  //   assert.isOk(result2.tx);
-  //   assert.equal(result2.tx.logs.length, 1); // no other event
-  //   assert.equal(result2.tx.logs[0].event, "RevokeSignToCancelAgreement");
-  // });
+    assert.isOk(result2);
+    assert.isOk(result2.tx);
+    assert.equal(result2.tx.logs.length, 1); // no other event
+    assert.equal(result2.tx.logs[0].event, "RevokeSignToCancelAgreement");
+  });
 
-  // it("sign to cancel agreement", async () => {
+  it("sign to cancel agreement", async () => {
 
-  //   const options = {
-  //     amountPerPeriod: web3.toWei(10),
-  //     beneficiaryAddress: helpers.SOME_ADDRESS,
-  //     cliffInPeriods: 0,
-  //     numOfAgreedPeriods: 1,
-  //     periodLength: 1,
-  //     returnOnCancelAddress: accounts[0],
-  //     signaturesReqToCancel: 1,
-  //     signers: [accounts[0]],
-  //     token: await dao.token.address,
-  //   };
+    const options = {
+      amountPerPeriod: web3.toWei(10),
+      beneficiaryAddress: helpers.SOME_ADDRESS,
+      cliffInPeriods: 0,
+      numOfAgreedPeriods: 1,
+      periodLength: 1,
+      returnOnCancelAddress: accounts[0],
+      signaturesReqToCancel: 1,
+      signers: [accounts[0]],
+      token: await dao.token.address,
+    };
 
-  //   const result = await createAgreement(options);
-  //   const agreementId = result.agreementId;
+    const result = await createAgreement(options);
+    const agreementId = result.agreementId;
 
-  //   const result2 = await vestingScheme.signToCancel({ agreementId });
+    const result2 = await vestingScheme.signToCancel({ agreementId });
 
-  //   assert.isOk(result2);
-  //   assert.isOk(result2.tx);
-  //   assert.equal(result2.tx.logs[0].event, "SignToCancelAgreement");
-  //   assert.equal(result2.tx.logs[1].event, "AgreementCancel");
-  // });
+    assert.isOk(result2);
+    assert.isOk(result2.tx);
+    assert.equal(result2.tx.logs[0].event, "SignToCancelAgreement");
+    assert.equal(result2.tx.logs[1].event, "AgreementCancel");
+  });
 
-  // it("create agreement", async () => {
+  it("create agreement", async () => {
 
-  //   const options = {
-  //     amountPerPeriod: web3.toWei(10),
-  //     beneficiaryAddress: helpers.SOME_ADDRESS,
-  //     cliffInPeriods: 0,
-  //     numOfAgreedPeriods: 1,
-  //     periodLength: 1,
-  //     returnOnCancelAddress: helpers.SOME_ADDRESS,
-  //     signaturesReqToCancel: 1,
-  //     signers: [accounts[0]],
-  //     token: await dao.token.address,
-  //   };
+    const options = {
+      amountPerPeriod: web3.toWei(10),
+      beneficiaryAddress: helpers.SOME_ADDRESS,
+      cliffInPeriods: 0,
+      numOfAgreedPeriods: 1,
+      periodLength: 1,
+      returnOnCancelAddress: helpers.SOME_ADDRESS,
+      signaturesReqToCancel: 1,
+      signers: [accounts[0]],
+      token: await dao.token.address,
+    };
 
-  //   const result = await createAgreement(options);
+    const result = await createAgreement(options);
 
-  //   assert.isOk(result);
-  //   assert.isOk(result.tx);
-  //   assert.isOk(result.agreementId);
-  //   assert(result.agreementId >= 0);
-  // });
+    assert.isOk(result);
+    assert.isOk(result.tx);
+    assert.isOk(result.agreementId);
+    assert(result.agreementId >= 0);
+  });
 
   it("propose agreement", async () => {
 
